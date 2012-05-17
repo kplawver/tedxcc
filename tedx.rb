6404049f379cb42e61aff62e9996f84ddff2a4a1
Kevin Lawver <lawver@gmail.com>
2012-05-17 10:17:26 -0400

    Getting ready for the live stream

--- tedx.rb
+++ tedx.rb
@@ -1,5 +1,6 @@
 class TedX < Sinatra::Base
   @@blog_url = "http://blog.tedxcreativecoast.com"
+  @@live = false
   
   # f = File.open("#{settings.public_folder}/index.html")
   # @@index_content = f.read
@@ -8,7 +9,10 @@ class TedX < Sinatra::Base
   set :static_cache_control, [:public, {:max_age => 2592000}]
   
   get '/' do
-    @live = false
+    @live = @@live
+    # if @live
+    #   redirect 'http://new.livestream.com/tedxcc/tedxcc2012'
+    # end
     cache_control :public, :max_age => 24000
     erb :index
   end
